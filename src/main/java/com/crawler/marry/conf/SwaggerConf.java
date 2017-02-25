0915ac4c58974eb0aa0c1ac3d392b1642d8a7251
xfz <xfz219@126.com>
2017-02-25 11:01:35 +0800

    买买买

--- src/main/java/com/crawler/marry/conf/SwaggerConf.java
+++ src/main/java/com/crawler/marry/conf/SwaggerConf.java
@@ -2,6 +2,7 @@ package com.crawler.marry.conf;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.async.DeferredResult;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -19,12 +20,14 @@ public class SwaggerConf {
 
     @Bean
     public Docket createRestApi(){
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("WebMagicTest.crawler.marry.web"))
-                .paths(PathSelectors.any())
-                .build();
+        // return new
+        // Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
+        // .paths(PathSelectors.regex("/credit/.*")).build();
+        return new Docket(DocumentationType.SWAGGER_2).groupName("demo").genericModelSubstitutes(DeferredResult.class)
+                // .genericModelSubstitutes(ResponseEntity.class)
+                .useDefaultResponseMessages(false).forCodeGeneration(true).pathMapping("/")// base，最终调用接口后会和paths拼接在一起
+                .select().paths((PathSelectors.regex("/marry.*")))// 过滤的接口
+                .build().apiInfo(apiInfo());
     }
 
     private ApiInfo apiInfo(){
